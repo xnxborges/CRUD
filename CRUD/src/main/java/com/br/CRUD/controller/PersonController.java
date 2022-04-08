b2f39b40121326142f7267e3e797c48369bf8be4
Natan Borges <natan.borges@capgemini.com>
2022-04-08 14:51:15 -0300

    Implementado o metodo FINDALL ńa URL para retorna uma lista de pessoas

--- CRUD/src/main/java/com/br/CRUD/controller/PersonController.java
+++ CRUD/src/main/java/com/br/CRUD/controller/PersonController.java
@@ -1,5 +1,7 @@
 package com.br.CRUD.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +19,14 @@ public class PersonController {
 	@Autowired
 	private PersonService services;
 
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return services.findAll();
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") String id) {
 		return services.findbyId(id);
 	}
+
 }
--- CRUD/src/main/java/com/br/CRUD/services/PersonService.java
+++ CRUD/src/main/java/com/br/CRUD/services/PersonService.java
@@ -1,5 +1,7 @@
 package com.br.CRUD.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Repository;
@@ -13,12 +15,34 @@ public class PersonService {
 
 	private final AtomicLong counter = new AtomicLong();
 
+	// Mock estático
 	public Person findbyId(String id) {
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
 		person.setFistName("Natan");
 		person.setLastName("Borges");
 		person.setAdress("Cotia-São Paulo-Brasil");
+		person.setGender("Male");
+		return person;
+	}
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+
+	}
+
+	// mock Dinâmico
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFistName("Person name" + i);
+		person.setLastName("Last name" + i);
+		person.setAdress("Some adress in Brazil" + i);
 		person.setGender("Male");
 		return person;
 	}
